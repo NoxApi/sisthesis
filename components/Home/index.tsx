5f7b7abc33ba55fe2bffff4b3652c1935a05528a
NoxApi <pcsnon@gmail.com>
2022-07-18 01:01:47 +0700

    fix the graph query method

--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,42 +1,54 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useLP } from "../../hooks"
 import { useGlobalContext } from "../../state"
 import { ApiEndpoint } from "../../config/constants/endpoint";
 //import context
 import { ApolloClient, InMemoryCache, gql ,useQuery } from '@apollo/client';
+import { setPriority } from "os";
 
 export const Home= () => {
     const {U} = ApiEndpoint()
     const {get} = useLP()!
     const {z,t} = useGlobalContext()!
     const [x,setx] = useState("kuy")
-    const [x2,setx2] = useState("AA000003")
-    const [url,seturl] = useState("") 
+    const [x2,setx2] = useState("ss")
+    const [url,seturl] = useState({url:"notfound"}) 
     const [time,settime] = useState(0) 
+    const [p,setp] = useState([]) 
     const URI = U[97]
-    async function getU(v:any){
+    async function getU(){
       const client = new ApolloClient({
       uri: URI,
       cache: new InMemoryCache()
     });
-    var start = Date.now();
-
-
+    
     const {data} = await client.query({
-      variables: {v},
       query: gql`
-        query getvalue($v: String){
-            urls (where:{id:$v}){
+        query getvalue{
+            urls{
+                id
                 url
         }   }
       `
     });
-    var end = Date.now(); // this happens AFTER you've fetched the data
-    console.log(data.urls[0].url)
-    console.log(v)
-    seturl(data.urls[0].url)
-    settime(end - start)
+     // this happens AFTER you've fetched the data
+    setp(data.urls)
+    console.log(data)
+    console.log(p)
+  
     }
+    async function query(v:any){
+      var start = Date.now();
+      const x = await p.filter( function(item: { id: any }){return (item.id ==v);} );
+      seturl(x[0])
+      console.log(x)
+      var end = Date.now();
+      settime(end - start)
+    }
+    useEffect(()=>{
+      getU()
+    },[])
+
     return (
       <div className="md:max-w-7xl mx-auto  px-8 h-[835px] w-full">
 
@@ -58,10 +70,10 @@ export const Home= () => {
                     onChange={(e)=>setx2(e.target.value)}
                     placeholder="enter code"
                     />
-        <button  className="button-gold w-[20%] m-[10px] h-[60px]" onClick={()=>getU(x2)}>
+        <button  className="button-gold w-[20%] m-[10px] h-[60px]" onClick={()=>query(x2)}>
             <p className="font-bold text-black">get URL by Code (The Graph)</p>
           </button>
-          <a>{url}</a>
+          <a>{url.url}</a>
           <a>{"excecute time = "+time+" ms"}</a>
         </div>
       </div>
